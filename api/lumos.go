ca201ec8183b1a7448fd67da10b02b19c7b4b2d9
Martin D'Agostino <martin.dagostino@klarna.com>
2022-04-02 23:00:08 +0200

    test notification

--- api/lumos.go
+++ api/lumos.go
@@ -8,12 +8,21 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 type Response struct {
 	Msg    string `json:"text"`
 	ChatID int64  `json:"chat_id"`
 	Method string `json:"method"`
+}
+
+var bot, _ = tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+var _, _ = Notify()
+
+func Notify() (tgbotapi.Message, error) {
+	var msg = tgbotapi.NewMessage(1929798658, "testing testing")
+	return bot.Send(msg)
 }
 
 func Reply(w http.ResponseWriter, r *http.Request) {
@@ -25,8 +34,7 @@ func Reply(w http.ResponseWriter, r *http.Request) {
 		log.Fatal("Error updating →", err)
 	}
 
-	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-	log.Printf("Chat: %d", update.Message.Chat.ID)
+	log.Printf("[%s@%d] %s", update.Message.From.UserName, update.Message.Chat.ID, update.Message.Text)
 
 	reply := processor.LookupReply(update)
 
