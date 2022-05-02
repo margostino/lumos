670e885149063ba385cc15b1ca7c3f78889f6d65
Martin D'Agostino <martin.dagostino@klarna.com>
2022-05-02 22:09:40 +0200

    debugging deployment

--- processor/start.go
+++ processor/start.go
@@ -2,7 +2,7 @@ package processor
 
 func StartReply(input string) (bool, string) {
 	if input == "start" {
-		return true, "👋   Welcome to Lumos!\n🌱   Raise awareness.\n🔊   Spread the word."
+		return true, "👋   Welcome to Lumos!\n🌱   xRaise awareness.\n🔊   Spread the word."
 	}
 	return false, ""
 }
