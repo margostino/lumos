52d666bc483ae2299e83039114490d288b4d8dc0
Martin D'Agostino <martin.dagostino@klarna.com>
2023-04-11 17:59:23 +0200

    ask name and obs

--- api/lumos.go
+++ api/lumos.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/margostino/lumos/common"
+	"github.com/margostino/lumos/datasource"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -19,40 +20,54 @@ type Response struct {
 
 var bot, _ = newBot()
 
+var variable *datasource.Variable
+
 func Reply(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
+
+	var update tgbotapi.Update
+	var reply string
+
 	w.Header().Add("Content-Type", "application/json")
 	body, _ := ioutil.ReadAll(r.Body)
-	var update tgbotapi.Update
+
 	if err := json.Unmarshal(body, &update); err != nil {
 		log.Fatal("Error updating →", err)
 	}
 
 	log.Printf("[%s@%d] %s", update.Message.From.UserName, update.Message.Chat.ID, update.Message.Text)
 
-	if update.Message.Location != nil {
-		geoData := fmt.Sprintf("Latitude: %f - Latitude: %f\n", update.Message.Location.Latitude, update.Message.Location.Longitude)
-		reply := geoData
-		log.Print(geoData)
-		data := Response{
-			Msg:    reply,
-			Method: "sendMessage",
-			ChatID: update.Message.Chat.ID,
-		}
-
-		message, _ := json.Marshal(data)
-		log.Printf("Response %s", string(message))
-		fmt.Fprintf(w, string(message))
-	} else {
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Send location")
-		//msg.ReplyToMessageID = update.Message.MessageID
+	if update.Message.Text == "/start" {
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Welcome to Lumos!")
 		btn := tgbotapi.KeyboardButton{
 			RequestLocation: true,
-			Text:            "Send location",
+			Text:            "Track location",
 		}
 		msg.ReplyMarkup = tgbotapi.NewReplyKeyboard([]tgbotapi.KeyboardButton{btn})
 		bot.Send(msg)
+	} else if update.Message.Location != nil {
+		latitude := update.Message.Location.Latitude
+		longitude := update.Message.Location.Longitude
+		variable = &datasource.Variable{
+			Latitude:  latitude,
+			Longitude: longitude,
+		}
+
+		reply = fmt.Sprintf("Latitude: %f - Longitude: %f\nSend variable name and observation separated by semicolon (e.g. some_name;this is a sample) do you want to register?\n", latitude, longitude)
+
+	} else if variable != nil {
+		reply = fmt.Sprintf("You sent variable name %s and observation %s", variable.Name, variable.Observation)
 	}
+
+	data := Response{
+		Msg:    reply,
+		Method: "sendMessage",
+		ChatID: update.Message.Chat.ID,
+	}
+
+	message, _ := json.Marshal(data)
+	log.Printf("Response %s", string(message))
+	fmt.Fprintf(w, string(message))
 
 	//reply := processor.LookupReply(update)
 	//
---
+++ datasource/model.go
@@ -0,0 +1,8 @@
+package datasource
+
+type Variable struct {
+	Name        string
+	Observation string
+	Longitude   float64
+	Latitude    float64
+}
